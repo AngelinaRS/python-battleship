ec959723716d3c13eb96bc6ca3f47382a26f730c
AngelinaRS <candy26y_@hotmail.com>
2015-10-29 11:08:54 -0600

    incompleto

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ def print_board():
     for o in BOARD:
         print " ".join(o)
 
-def random_file():
+def random_row():
     return random.randint(1, 10)
 
 def random_column():
@@ -42,32 +42,77 @@ def random_column():
 
 def ship_of_one():
 
-    board()
-    file_board = random_file()
-    column_board = random_column()
-    return file_board, column_board
+    #board()
+    row_one_h = random_row()
+    column_one_h = random_column()
+    return row_one_h, column_one_h
+
+def put_ship_of_two_horizontal():
+
+    row_two_h1 = random_row()
+    column_two_h1 = random_column()
+
+    column_two_h2 = column_two_h1+1
+
+    if column_two_h1 == 10:
+        column_two_h2 = column_two_h1-1
+
+    return row_two_h1, column_two_h1, column_two_h2
+
+#def ship_one(guess_row, guess_column, row_one_h, column_one_h):
+
 
 def ask_to_the_user():
-    file_board, column_board = ship_of_one()
-    print file_board
-    print column_board
+    board()
+    row_one_h, column_one_h = ship_of_one()
+    row_two_h1, column_two_h1, column_two_h2 = put_ship_of_two_horizontal()
+
+    print "ship of one"
+    print row_one_h
+    print column_one_h
+
+    print "ship of two"
+    print row_two_h1
+    print column_two_h1
+    print ""
+    print row_two_h1
+    print column_two_h2
 
     while True:
 
-        guess_file = input("\nGuess the file: ")
+        guess_row = input("\nGuess the row: ")
         guess_column = input("Guess the column: ")
 
-        if guess_file == file_board and guess_column == column_board:
-            BOARD[guess_file-1][guess_column-1] = "*"
+        if guess_row == row_one_h and guess_column == column_one_h:
+            BOARD[guess_row-1][guess_column-1] = "*"
             reset()
-            print "Guessed"
+            print "You have sunk the ship of one piece"
             print_board()
-            press_enter()
+
+        elif (guess_row == row_two_h1) and (guess_column == column_two_h1):
+            BOARD[guess_row-1][guess_column-1] = "*"
+            reset()
+            print_board()
+        elif (guess_row == row_two_h1) and (guess_column == column_two_h2):
+            BOARD[guess_row-1][guess_column-1] = "*"
+            reset()
+            print_board()
         else:
-            BOARD[guess_file-1][guess_column-1] = "X"
-            reset()
-            print "No"
-            print_board()
+            if (guess_row < 1 or guess_row > 10) or (guess_column < 1 or guess_column > 10):
+                reset()
+                print "It is not in the ocean"
+                print_board()
+
+            elif (BOARD[guess_row-1][guess_column-1] == "X"):
+                reset()
+                print "Already You have written those coordinates"
+                print_board()
+
+            else:
+                BOARD[guess_row-1][guess_column-1] = "X"
+                reset()
+                print "Try again "
+                print_board()
 
 def menu_print():
     """This saves the instructions of the game"""
